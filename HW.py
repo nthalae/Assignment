35951810d3f709293d776386b392081f0bb7ad62
emmanuel <enthala@aims.edu.gh>
2014-08-28 15:28:37 +0000

    the file

--- HW.py
+++ HW.py
@@ -2,5 +2,6 @@ __author__ = 'emmanuel'
 print("Hello People")
 print("A Fly in a Cup of Tea")
 print("Hello Ghana")
-
-
+print("Hello Ghana")
+print("some changes")
+print("change further but bot working here")
