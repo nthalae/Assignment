4ef7662ed3eb69e19870abe34a395d4b75e01e17
emmanuel <enthala@aims.edu.gh>
2014-08-29 20:21:37 +0000

    the file

--- HW.py
+++ HW.py
@@ -5,4 +5,30 @@ print("Hello Ghana")
 print("Hello Ghana")
 print("some changes")
 print("change further but bot working here")
-print("Hello Malawi hallo")
+
+
+from numbers import Number
+from numpy import *
+
+def square_perimeter(side : Number) -> Number:
+    """
+    calculate perimeter of a square from side length.
+    @param side: the side length
+    12
+    """
+    return 4*side
+print (square_perimeter(3))
+
+
+    @return: the parameter (same unit as the length)
+    >>> square_perimeter(3)
+def circle_area(radius : Number) -> Number:
+    """
+    calculate the area of a circle from radius
+    @param radius: the circle's radius
+    @return: the parameter (same unit as the radius)
+    >>> circle_area(7)
+    154
+    """
+    return pi*radius*radius
+print (circle_area(7))
